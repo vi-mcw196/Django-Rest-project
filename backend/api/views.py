6ee91746bff5d5bf00b0441a3e723c05c7e0fa90
vi-mcw196 <moldlova50@gmail.com>
2023-02-14 15:02:23 +0100

    init products app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse, HttpRequest
+
 
 # Create your views here.
+def api_home(request: HttpRequest, *args, **kwargs):
+    body = request.body.decode('utf-8')
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.META
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,17 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000/"
-endpoint2 = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"  # http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint)  # HTTP request
-print(get_response.text)
-# print(get_response.json())
+get_response = requests.post(endpoint,
+                             json={"title": "Abc123", "content": "Hello world", "price": "abc134"})  # HTTP Request
+# print(get_response.headers)
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Nototion ~ Python Dict
+print(get_response.json())
+# print(get_response.status_code)
