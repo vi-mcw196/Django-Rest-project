4656d20238e947078f549a6a5265304a8d219e09
vi-mcw196 <moldlova50@gmail.com>
2023-02-15 13:37:30 +0100

    feat!: ProductCreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
